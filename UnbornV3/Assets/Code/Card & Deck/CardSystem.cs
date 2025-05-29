0564e5289b50d3f67edc4a7af5f2a044b308fc48
Ziondaman_SV <121035557+ZiondamanSE@users.noreply.github.com>
2025-05-29 11:40:21 +0200

    new changes to the dec system and starting on the card system

---
+++ UnbornV3/Assets/Code/Card & Deck/CardSystem.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CardSystem : MonoBehaviour
+{
+    public float movmentSystem;
+    public float followMouseCurserSpeed;
+    public bool Home;
+
+    public Transform Deck;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- UnbornV3/Assets/Code/Card & Deck/DeckSystem.cs
+++ UnbornV3/Assets/Code/Card & Deck/DeckSystem.cs
@@ -1,55 +1,41 @@
 using System.Collections;
 using UnityEngine;
-
 public class DeckSystem : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
-
     public int cardsAmount;
     public int cardsMax;
     public float cardsRefreshRateAmount;
     public Transform spawningPoint;
-
     private bool needCards;
     private bool isAddingCards = false;
-
     void Start()
     {
         cardsAmount = 0;
         needCards = true;
-
         if (gameManager == null)
             gameManager = FindObjectOfType<GameManager>();
     }
-
-
     void Update()
     {
         if (cardsAmount < cardsMax)
             needCards = true;
         else
             needCards = false;
-
         if (needCards && !isAddingCards)
             StartCoroutine(AddCardsToDeck());
     }
-
     IEnumerator AddCardsToDeck()
     {
         isAddingCards = true;
-
-        for (int i = cardsAmount; i < cardsMax; i++)
+        for (int i = cardsAmount; i < cardsMax; i++) // spawns the cards
         {
-            GameObject newCard = gameManager.CardSummon();
-            if (newCard != null && spawningPoint != null)
-                Instantiate(newCard, spawningPoint.position, spawningPoint.rotation);
+            GameObject newCard = gameManager.CardSummon(); // gets the card
+            if (newCard != null && spawningPoint != null) // summons the card as child
+                Instantiate(newCard, spawningPoint.position, spawningPoint.rotation, spawningPoint);
             cardsAmount++;
-
-            Debug.Log(i + " = i");
-            Debug.Log(cardsAmount + " = i");
             yield return new WaitForSeconds(cardsRefreshRateAmount);
         }
-
         needCards = false;
         isAddingCards = false;
         yield return null;
